7dff3e771788e166924b608c6691fc654408e74a
franc454 <franc454@umn.edu>
2017-03-03 12:07:38 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Keenen Francois-King
 #Project 1
 
-
+import liquid_water_content
 
 #user input from data that is collected at the site of measurement
 Im1 = float(input("What is the normalized intensity measured at GPS1 above the snowpack (dB)?\n"))
@@ -9,8 +9,6 @@ Im23 = float(input("What is the normalized mean intensity of GPS2 and GPS3 below
 d = float(input("What is the snow depth? (meters)\n"))
 theta_elev = float(input("What is the elevation angle received at GPS1 from the satellite? (degrees)\n"))
 print("\n")
-
-import liquid_water_content
 
 solve = liquid_water_content.LWE(Im1, Im23, d, theta_elev)
 solve.solve_equations()
